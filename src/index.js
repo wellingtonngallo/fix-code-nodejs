01a9e5e483084ad3828dafa6f278e21aa1ccf34d
Wellington Gallo <wellinton.gallo77@gmail.com>
2022-01-26 14:39:48 -0300

    fix: all code

--- src/index.js
+++ src/index.js
@@ -14,7 +14,6 @@ app.get("/repositories", (request, response) => {
 
 app.post("/repositories", (request, response) => {
   const { title, url, techs } = request.body
-
   const repository = {
     id: uuid(),
     title,
@@ -23,24 +22,32 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository);
+
+  return response.status(201).send(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const { title, url, techs } = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
+  }
+
+  const updatedRepository = {
+    title,
+    url, 
+    techs
   }
 
   const repository = { ...repositories[repositoryIndex], ...updatedRepository };
 
   repositories[repositoryIndex] = repository;
 
-  return response.json(repository);
+  return response.status(201).json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
@@ -48,27 +55,25 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
   repositories.splice(repositoryIndex, 1);
 
   return response.status(204).send();
+  
 });
 
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
-
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex < 0) {
-    return response.status(404).json({ error: "Repository not found" });
-  }
+  if (repositoryIndex < 0) return response.status(404).json({ error: "Repository not found" });
+  
+  ++repositories[repositoryIndex].likes
 
-  const likes = ++repositories[repositoryIndex].likes;
-
-  return response.json('likes');
+  return response.status(201).send({likes: repositories[repositoryIndex].likes});
 });
 
 module.exports = app;
